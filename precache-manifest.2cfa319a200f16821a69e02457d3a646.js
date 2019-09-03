f5a000c734a5dd9128a715e1c4f120eccad6c2e4
unknown <tandrushko@romexsoft.com>
2019-09-03 15:06:35 +0300

    Updates

---
+++ precache-manifest.2cfa319a200f16821a69e02457d3a646.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b63c96eda2c27857431e479ea3b1e13",
+    "url": "/calculate/index.html"
+  },
+  {
+    "revision": "7773d0cea131dcf9f77b",
+    "url": "/calculate/static/css/main.76981704.chunk.css"
+  },
+  {
+    "revision": "29902c9ae51716c78eea",
+    "url": "/calculate/static/js/2.17adfd40.chunk.js"
+  },
+  {
+    "revision": "7773d0cea131dcf9f77b",
+    "url": "/calculate/static/js/main.c019fb9a.chunk.js"
+  },
+  {
+    "revision": "635e663c758f2e4105db",
+    "url": "/calculate/static/js/runtime~main.d1a39610.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculate/static/media/logo.5d5d9eef.svg"
+  }
+]);
